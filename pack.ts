75d6d406bf42da54c06db1261606ebb0a0215ad8
Gary Shen <gary@coda.io>
2023-04-29 21:00:20 -0700

    add card

--- pack.ts
+++ pack.ts
@@ -30,7 +30,7 @@ function rowToBook(bookRow, shelf) {
     link: `https://goodreads.com${relLink}`,
     coverUrl: bookRow.querySelector('.cover').querySelector('.value').querySelector('div').querySelector('a').querySelector('img').getAttribute('src').trim(),
     title: bookRow.querySelector('.title').querySelector('.value').querySelector('a').text.trim(),
-    author: bookRow.querySelector('.author').querySelector('.value').querySelector('a').text.trim(),
+    firstAuthor: bookRow.querySelector('.author').querySelector('.value').querySelector('a').text.trim(),
     isbn: bookRow.querySelector('.isbn').querySelector('.value').text.trim(),
     isbn13: bookRow.querySelector('.isbn13').querySelector('.value').text.trim(),
     rating: RATING_MAP[bookRow.querySelector('.rating').querySelector('.value').querySelector('.staticStars').getAttribute('title')?.trim()],
@@ -41,12 +41,23 @@ function rowToBook(bookRow, shelf) {
   }
 }
 
+function pageToBook(bookResponse, url) {
+  return {
+    link: url,
+    coverUrl: bookResponse.querySelector('.BookCover__image').querySelector('.ResponsiveImage').getAttribute('src').trim(),
+    title: bookResponse.querySelector('.BookPageTitleSection__title').querySelector('.Text__title1').text.trim(),
+    firstAuthor: bookResponse.querySelector('.BookPageMetadataSection__contributor').querySelector('.ContributorLinksList').querySelector('.ContributorLink__name').text.trim(),
+    isbn: JSON.parse(bookResponse.querySelector('[type="application/ld+json"]').text.trim())["isbn"],
+    avgRating: parseFloat(bookResponse.querySelector('.RatingStatistics__rating').text.trim()),
+  }
+}
+
 const BookSchema = coda.makeObjectSchema({
   properties: {
     link: { type: coda.ValueType.String, codaType: coda.ValueHintType.Url },
     coverUrl: { type: coda.ValueType.String, codaType: coda.ValueHintType.ImageReference},
     title: { type: coda.ValueType.String },
-    author: { type: coda.ValueType.String },
+    firstAuthor: { type: coda.ValueType.String },
     isbn: { type: coda.ValueType.String },
     isbn13: { type: coda.ValueType.String },
     rating: { type: coda.ValueType.Number },
@@ -56,8 +67,42 @@ const BookSchema = coda.makeObjectSchema({
     dateAdded: { type: coda.ValueType.String },
   },
   displayProperty: "title",
+  linkProperty: "link",
+  subtitleProperties: ["firstAuthor", "rating", "shelf"],
+  imageProperty: "coverUrl",
   idProperty: "link",
-  featuredProperties: ["title", "author", "rating", "shelf", "link"]
+  featuredProperties: ["title", "firstAuthor", "rating", "shelf", "link"]
+});
+
+pack.addFormula({
+  name: "book",
+  description: "get a book by link",
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "link",
+      description: "goodreads book link",
+    })
+  ],
+  resultType: coda.ValueType.Object,
+  schema: BookSchema,
+  execute: async function ([link], context) {
+    const url = link.split(/[?#]/)[0];
+    const response = await context.fetcher.fetch({
+      method: "GET",
+      url,
+    });
+    return pageToBook(parse(response.body), url)
+  }
+});
+
+pack.addColumnFormat({
+  name: "book",
+  formulaName: "book",
+  instructions: "get a book by link",
+  matchers: [
+    /^https:\/\/.+\.goodreads\.com\/book\/show\/[0-9]*.*/i,
+  ]
 });
 
 pack.addSyncTable({
